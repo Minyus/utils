7baf9d00e7de7d19a767cb5275ea6a4b22099856
Minyus <Minyus@github.com>
2024-09-09 00:01:29 +0800

    Use if __name__ == "__main__":

--- batch_vector_search/weaviate_batch_vector_search.py
+++ batch_vector_search/weaviate_batch_vector_search.py
@@ -86,99 +86,99 @@ def show(*args, **kwargs):
     print("@@@@", *args, **kwargs)
 
 
-weaviate_version = "1.26.4"
+if __name__ == "__main__":
+    weaviate_version = "1.26.4"
 
-client = weaviate.WeaviateClient(
-    embedded_options=EmbeddedOptions(
-        additional_env_vars={
-            "ASYNC_INDEXING": "false",
-            "DISABLE_TELEMETRY": "true",
-            "DISK_USE_READONLY_PERCENTAGE": "98",
-            "DISK_USE_WARNING_PERCENTAGE": "90",
-            "ENABLE_MODULES": "backup-filesystem",
-            # "LIMIT_RESOURCES": "true",
-            "LOG_LEVEL": "warning",
-            "MODULES_CLIENT_TIMEOUT": "10s",
-            "PERSISTENCE_HNSW_MAX_LOG_SIZE": "500MiB",
-            "PERSISTENCE_LSM_ACCESS_STRATEGY": "mmap",
-            "PERSISTENCE_LSM_MAX_SEGMENT_SIZE": "20GiB",
-            "QUERY_SLOW_LOG_ENABLED": "true",
-            "QUERY_SLOW_LOG_THRESHOLD": "10s",
-            "REINDEX_SET_TO_ROARINGSET_AT_STARTUP": "true",
-            "BACKUP_FILESYSTEM_PATH": "/tmp/backups",
-        },
-        version=weaviate_version,
-        port=8079,
-    )
-)
-
-
-client.connect()
-
-try:
-    collection_name = "CollectionFoo"
-
-    if client.collections.exists(collection_name):
-        collection = client.collections.get(collection_name)
-    else:
-        collection = client.collections.create(
-            name=collection_name,
-            vector_index_config=Configure.VectorIndex.hnsw(
-                distance_metric=VectorDistances.COSINE
-            ),
-            properties=[
-                Property(name="item", data_type=DataType.TEXT),
-                Property(name="price", data_type=DataType.NUMBER),
-            ],
+    client = weaviate.WeaviateClient(
+        embedded_options=EmbeddedOptions(
+            additional_env_vars={
+                "ASYNC_INDEXING": "false",
+                "DISABLE_TELEMETRY": "true",
+                "DISK_USE_READONLY_PERCENTAGE": "98",
+                "DISK_USE_WARNING_PERCENTAGE": "90",
+                "ENABLE_MODULES": "backup-filesystem",
+                # "LIMIT_RESOURCES": "true",
+                "LOG_LEVEL": "warning",
+                "MODULES_CLIENT_TIMEOUT": "10s",
+                "PERSISTENCE_HNSW_MAX_LOG_SIZE": "500MiB",
+                "PERSISTENCE_LSM_ACCESS_STRATEGY": "mmap",
+                "PERSISTENCE_LSM_MAX_SEGMENT_SIZE": "20GiB",
+                "QUERY_SLOW_LOG_ENABLED": "true",
+                "QUERY_SLOW_LOG_THRESHOLD": "10s",
+                "REINDEX_SET_TO_ROARINGSET_AT_STARTUP": "true",
+                "BACKUP_FILESYSTEM_PATH": "/tmp/backups",
+            },
+            version=weaviate_version,
+            port=8079,
         )
-
-        data_rows = [
-            {
-                "vector": [i / 256, 1 - i / 256],
-                "properties": {"item": f"{i:03d}", "price": i},
-            }
-            for i in range(256)
-        ]
-
-        with collection.batch.dynamic() as batch:
-            for data_row in data_rows:
-                batch.add_object(**data_row)
-
-    show(client.cluster.nodes(output="verbose"))
-
-    query = [0.7, 0.7]
-
-    # response = collection.query.near_vector(
-    # near_vector=query,
-    # limit=1,
-    # return_metadata=MetadataQuery(distance=True),
-    # filters=Filter.by_property("price").greater_than(0),
-    # )
-    # response_dict = response_to_dict(response, selecting_properties={"item"})
-    # show(response_dict)
-
-    query_batch = [query for _ in range(3)]
-
-    def query_func(x):
-        return response_to_dict(x, selecting_properties={"item"})
-
-    batch_results = batch_search(
-        collection,
-        query_batch,
-        search_kwargs=dict(
-            limit=1,
-            return_metadata=MetadataQuery(distance=True),
-            filters=Filter.by_property("price").greater_than(0),
-        ),
-        query_fn=query_func,
-        loader_kwargs={"batch_size": 100, "num_workers": 2},
-        tqdm_kwargs={"mininterval": 1.0},
     )
-    show(batch_results)
 
-except:
-    raise
-finally:
-    show("closing client...")
-    client.close()
-    show("closed client.")
+    client.connect()
+
+    try:
+        collection_name = "CollectionFoo"
+
+        if client.collections.exists(collection_name):
+            collection = client.collections.get(collection_name)
+        else:
+            collection = client.collections.create(
+                name=collection_name,
+                vector_index_config=Configure.VectorIndex.hnsw(
+                    distance_metric=VectorDistances.COSINE
+                ),
+                properties=[
+                    Property(name="item", data_type=DataType.TEXT),
+                    Property(name="price", data_type=DataType.NUMBER),
+                ],
+            )
+
+            data_rows = [
+                {
+                    "vector": [i / 256, 1 - i / 256],
+                    "properties": {"item": f"{i:03d}", "price": i},
+                }
+                for i in range(256)
+            ]
+
+            with collection.batch.dynamic() as batch:
+                for data_row in data_rows:
+                    batch.add_object(**data_row)
+
+        show(client.cluster.nodes(output="verbose"))
+
+        query = [0.7, 0.7]
+
+        # response = collection.query.near_vector(
+        # near_vector=query,
+        # limit=1,
+        # return_metadata=MetadataQuery(distance=True),
+        # filters=Filter.by_property("price").greater_than(0),
+        # )
+        # response_dict = response_to_dict(response, selecting_properties={"item"})
+        # show(response_dict)
+
+        query_batch = [query for _ in range(3)]
+
+        def query_func(x):
+            return response_to_dict(x, selecting_properties={"item"})
+
+        batch_results = batch_search(
+            collection,
+            query_batch,
+            search_kwargs=dict(
+                limit=1,
+                return_metadata=MetadataQuery(distance=True),
+                filters=Filter.by_property("price").greater_than(0),
+            ),
+            query_fn=query_func,
+            loader_kwargs={"batch_size": 100, "num_workers": 2},
+            tqdm_kwargs={"mininterval": 1.0},
+        )
+        show(batch_results)
+
+    except:
+        raise
+    finally:
+        show("closing client...")
+        client.close()
+        show("closed client.")
